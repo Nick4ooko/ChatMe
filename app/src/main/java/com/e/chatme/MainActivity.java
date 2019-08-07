ad2d32fea1538d23b8b1c2af94764ba1c0336784
AC03 <oderocharlesouma@gmail.com>
2019-08-07 18:20:33 +0300

    Initial commit

---
+++ app/src/main/java/com/e/chatme/MainActivity.java
@@ -0,0 +1,27 @@
+package com.e.chatme;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    private Button signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        final Button signup =(Button)findViewById(R.id.bt_sign_up_id);
+        signup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                sign();
+            }
+        });
+    }
+
+    private void sign() {
+    }
+}
